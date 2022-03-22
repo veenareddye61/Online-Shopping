3a6c218ff587f6bb3a52d2248ad1b1359aebe664
veenareddye61 <101815704+veenareddye61@users.noreply.github.com>
2022-03-22 19:44:56 +0530

    Update checkout.js

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -2,9 +2,9 @@ var mainCartDiv= $("#main-cart-div")
 var cartDiv1 = $("#cart-div1")
 /*var cartDiv2 = document.getElementById("cart-div2")*/
 
-
+var prodCard
 prodCard=JSON.parse(localStorage.getItem("prodCard"));
-
+console.log(prodCard)
 $('#itemCount').text(prodCard.length)
 $("#cart-count").text(prodCard.length);
 
@@ -13,11 +13,33 @@ var sum=0
 /*cartDivProd2 = document.createElement("div")*/
 
 
+var prodCard2 = [];
+var number=0
+for(i=0;i<prodCard.length;i++){
+    sum+=parseInt(prodCard[i].price);
+    $("#totamt2").text(sum);
+    var number=parseInt(prodCard[i].numberOf)
+    console.log(number)
+    if(number>1)
+    {
+        prodCard2.pop();
+    }
+   prodCard2.push(prodCard[i])
+}
+console.log(prodCard2)
+for(j=0;j<prodCard2.length;j++){
+     cartDivProd =`<div class="chcard"><div class="chcard1"><img class="prodImg" src="${prodCard2[j].src}" ></img>
+</div>
+<div class="chcard2"><h4 class="prodName">${prodCard2[j].name}</h4>
+<p class= "prodNum">${'x'+prodCard2[j].numberOf}</p>
+<p class="prodPrice">${'Amount Rs:'+(prodCard2[j].numberOf*prodCard2[j].price)}</p></div></div>`
+cartDiv1.append(cartDivProd)
+}
 
 
-for(i=1;i<=prodCard.length;i++){
-if(prodCard[i].numberOf > 1){
-cartDivProd =`<div class="chcard"><div class="chcard1"><img class="prodImg" src="${prodCard[i].src}" ></img>
+
+
+/*cartDivProd =`<div class="chcard"><div class="chcard1"><img class="prodImg" src="${prodCard[i].src}" ></img>
 </div>
 <div class="chcard2"><h4 class="prodName">${prodCard[i].name}</h4>
 <p class= "prodNum">${'x'+prodCard[i].numberOf}</p>
@@ -33,7 +55,7 @@ cartDiv1.append(cartDivProd)
 sum+=parseInt(prodCard[i-1].price);
 $("#totamt2").text(sum);
 }
-console.log(cartDivProd.length)
+console.log(cartDivProd.length)*/
 function orderPlaced(){
     
     $("#cart-count").text("0");
