bda403f72b3eeea57dd8ec94c7e7293c5066124f
veenareddye61 <101815704+veenareddye61@users.noreply.github.com>
2022-03-29 21:10:08 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,31 @@
+var footerDiv = $("footer")
+var footerData = `<div>
+<p class="footer-heading">Online Store</p>
+<a href="/" class="footer-link">Men Clothing</a>
+<a href="/" class="footer-link">Women Clothing</a>
+<a href="/" class="footer-link">Men Accessories</a>
+<a href="/" class="footer-link">Women Accessories</a>
+</div>
+<div>
+<p class="footer-heading">Helpful Links</p>
+<a href="/" class="footer-link">Home</a>
+<a href="/" class="footer-link">About</a>
+<a href="/" class="footer-link">Contact</a>
+</div>
+<div>
+<p class="footer-heading">Partners</p>
+<a href="/" class="footer-link">Zara</a>
+<a href="/" class="footer-link">Pantaloons</a>
+<a href="/" class="footer-link">Levis</a>
+<a href="/" class="footer-link">UCB</a>
+<a href="/" class="footer-link">+ Many More</a>
+</div>
+<div>
+<p class="footer-heading">Address</p>
+<p href="/" class="footer-link">Building 101</p>
+<p href="/" class="footer-link">Central Avenue</p>
+<p href="/" class="footer-link">LA - 902722</p>
+<p href="/" class="footer-link">United States</p>
+</div>`
+
+footerDiv.append(footerData)
